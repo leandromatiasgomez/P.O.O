276469192e526e0a6f99bf0c5ebee42a44c3cff8
JoaquinReyes2905 <Mikemoble2905@gmail.com>
2023-07-06 16:01:44 -0300

    .

---
+++ Ejercicios/ejer06/ejer06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro1 = new Libro(001, "Jose", "A", 120);
+            Libro libro2 = new Libro(002, "Maria", "B", 200);
+
+            Console.WriteLine(libro1.infoLibro());
+            Console.WriteLine(libro2.infoLibro());
+
+            if (libro1.getNumeroPaginas < libro2.getNumeroPaginas)
+            {
+                Console.WriteLine("El libro con más paginas es: " + libro2.infoLibro());
+            }
+            else if (libro1.getNumeroPaginas > libro2.getNumeroPaginas)
+            {
+                Console.WriteLine("El libro con más paginas es: " + libro1.infoLibro());
+            }
+            else
+            {
+                Console.WriteLine("Ambos libros tiene la misma cantidad de paginas");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
