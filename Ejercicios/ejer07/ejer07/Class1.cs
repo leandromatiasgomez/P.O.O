96400c49f07bc652bfb47789f71d82ac46c70d99
lean <matigom13@gmail.com>
2023-07-06 16:53:47 -0300

    .

---
+++ Ejercicios/ejer07/ejer07/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer07
+{
+    internal class Raices
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public Raices (double a, double b, double c) 
+        {
+            this.a = a; 
+            this.b = b; 
+            this.c = c;
+        }
+
+        
+
+        public double getA
+        {
+            get
+            {
+                return this.a;
+            }
+        }
+
+        public double getB
+        {
+            get
+            {
+                return this.b;
+            }
+        }
+
+        public double getC
+        {
+            get 
+            { 
+                return this.c;
+            }
+        }
+
+        public double getDiscriminante()
+        {
+            return((getB * 2) - 4 * getA * getC);
+        }
+    }
+}
---
+++ Ejercicios/ejer07/ejer07/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_obligatorio_03
+{
+    class Password
+    {
+        public string contraseña = "pepe_elpro82";
+        public int longitud = 12;
+
+
+        public Password()
+        {
+        }
+        public Password(int longitud)
+        {
+            this.longitud = longitud;
+        }
+        public Password(string contraseña)
+        {
+            this.contraseña = contraseña;
+        }
+        public string Contraseña
+        {
+            get { return contraseña; }
+
+        }
+        public int Longitud
+        {
+            get { return longitud; }
+            set { longitud = value; }
+        }
+        public bool esFuerte()
+        {
+
+            return false;
+        }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Password[] contra = new Password[1];
+            contra[0] = new Password("holakheace");
+            Console.ReadKey();
+        }
+    }
+}
+
