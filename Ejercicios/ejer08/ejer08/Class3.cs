d6cfbc262d35acc1aa32da9211132f0eb7359a38
Lean <matigom13@gmail.com>
2023-07-14 14:53:09 -0300

    .

---
+++ Ejercicios/ejer08/ejer08/Class3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer08
+{
+    internal class Aulas
+    {
+        private int numeroDeAula;
+        private int capacidad;
+        private string materia;
+
+        public Aulas(int numeroDeAula, int capacidad, string materia)
+        {
+            this.numeroDeAula = numeroDeAula;
+            this.capacidad = capacidad;
+            comprobarMateria(materia);
+        }
+
+        public void comprobarMateria(string materia)
+        {
+            string materiasPosibles = "MATEMATICASFILOSOFIAFISICA";
+            if (materiasPosibles.Contains(materia.ToUpper()))
+            {
+                this.materia = materia;
+            }
+            else
+            {
+                this.materia = "Matematicas";
+            }
+        }
+
+        
+
+    }
+}
---
+++ Ejercicios/ejer08/ejer08/Class4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer08
+{
+    interface IPrecencia
+    {
+        string precencia(int faltas, int diasDeClases);
+    }
+}
